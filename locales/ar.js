1740d1842e9658252b1d657156facce699758385
hossam mahmoud <74540341+hossammahoud@users.noreply.github.com>
2023-07-16 20:06:17 +0300

    Add files via upload

---
+++ locales/ar.js
@@ -0,0 +1,13 @@
+export default {
+  navbar: {
+    feature: "الخصائص",
+    aboutus: "عن الشركة",
+    whoWeServe: "التحديات",
+    FAQ: "الاسئلة الشائعة",
+    contactUs: "اتصل بنا"
+  },
+  footer: {
+    copyright: "حقوق النشر",
+    allRights: "بروبرتي نت ، جميع الحقوق محفوظة."
+  }
+}
---
+++ locales/en.js
@@ -0,0 +1,13 @@
+export default {
+  navbar: {
+    feature: "Feature showcase",
+    aboutus: "About us",
+    whoWeServe: "who We Serve?",
+    FAQ: "FAQ",
+    contactUs: "Contact Us"
+  },
+  footer: {
+    copyright: "Copyright",
+    allRights: "PropertyNet, All Rights Reserved."
+  }
+}
